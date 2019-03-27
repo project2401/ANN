cc43b1343b681bc207564f9c878f1db1f5c20f7c
project2401 <igor.it.step@gmail.com>
2019-03-27 16:03:39 +0200

    Исправил недочеты при инициализации класса

--- np.py
+++ np.py
@@ -17,55 +17,50 @@ class Ann(object):
         # self.num4 = int(data_new[0][3])
         self.learning_rate = np.array([rate])
         self.sigmoid_mapper = np.vectorize(self.sigmoid)
+        self.weights_input_h1 = [0.1, 0.57, 0.44]
+        self.weights_input_h2 = [0.8, 0.34, 0.34]
+        self.weights_input_h3 = [0.88, 0.21, 0.37]
+        self.weights_input_h4 = [0.44, 0.57, 0.39]
+        self.weights_input_h = np.array([self.weights_input_h1, self.weights_input_h2, self.weights_input_h3, self.weights_input_h4])
+        self.weights_h_to_o1 = np.array([0.96, 0.15, 0.35, 0.45])   
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
     def predict(self, inputs):
         # inputs = np.array([num1, num2, num3])
-        weights_input_h1 = [0.1, 0.57, 0.44]
-        weights_input_h2 = [0.8, 0.34, 0.34]
-        weights_input_h3 = [0.88, 0.21, 0.37]
-        weights_input_h4 = [0.44, 0.57, 0.39]
-        weights_input_h = np.array([weights_input_h1, weights_input_h2, weights_input_h3, weights_input_h4])
-        weights_h_to_o1 = np.array([0.96, 0.15, 0.35, 0.45])
-        
-        h_input = np.dot(weights_input_h, inputs)
+        h_input = np.dot(self.weights_input_h, inputs)
         print("h_input = " +str(h_input))
         h_output = self.sigmoid_mapper(h_input)
         print("h_output = "+str(h_output))
 
-        o1_input = np.dot(weights_h_to_o1, h_output)
+        o1_input = np.dot(self.weights_h_to_o1, h_output)
         print("o1_input = " + str(o1_input))
         o1_output = self.sigmoid_mapper(o1_input)
         print('o1_output = ' + str(o1_output))
         return o1_output
 
-    def tarin(self, inputs, num4):
-        weights_input_h1 = [0.1, 0.57, 0.44]
-        weights_input_h2 = [0.8, 0.34, 0.34]
-        weights_input_h3 = [0.88, 0.21, 0.37]
-        weights_input_h4 = [0.44, 0.57, 0.39]
-        weights_input_h = np.array([weights_input_h1, weights_input_h2, weights_input_h3, weights_input_h4])
-        weights_h_to_o1 = np.array([0.96, 0.15, 0.35, 0.45])
-        
-        h_input = np.dot(weights_input_h, inputs)
+    def train(self, inputs, num4):
+        h_input = np.dot(self.weights_input_h, inputs)
         print("h_input = " +str(h_input))
         h_output = self.sigmoid_mapper(h_input)
         print("h_output = "+str(h_output))
 
-        o1_input = np.dot(weights_h_to_o1, h_output)
+        o1_input = np.dot(self.weights_h_to_o1, h_output)
         print("o1_input = " + str(o1_input))
         o1_output = self.sigmoid_mapper(o1_input)
         print('o1_output = ' + str(o1_output))
         actual_predict = o1_output
 
-        error = np.array([actual_predict - num4])
+        error = np.array([actual_predict - num4]) #ошибка при вычислении 
         print('Error = '+str(error))
         sigmoidDX = actual_predict * (1 - actual_predict)
         weights_delta = error*sigmoidDX
         print('weights_delta = '+str(weights_delta))
-        
+        self.weights_input_h -= (np.dot(weights_delta, h_output.reshape(1, len(h_output)))) * self.learning_rate
+
+
+
 
 
 
